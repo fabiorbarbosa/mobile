e4420b28bf1350cb2685542538afe99ec1f25705
Fábio Barbosa <fabio.b@uninove.edu.br>
2016-05-02 19:33:50 -0300

    $ionicModal
    Criação do modal para criar a task;

--- TaskList/www/js/controller.js
+++ TaskList/www/js/controller.js
@@ -1,8 +1,21 @@
 'use strict'
 
-app.controller('mainCtrl', function($scope, servTaskList){
+app.controller('mainCtrl', function($scope, $ionicModal, servTaskList){
+
+  $scope.formTaskList = [];
+
   $scope.taskLists = [];
+
   $scope.addTaskList = function(){
-    $scope.taskLists.push(servTaskList.addTaskList('testestes'));
+    $scope.taskLists.push(servTaskList.addTaskList($scope.formTaskList.task));
+    $scope.modal.hide();
+    $scope.formTaskList.task = '';
   };
-})
+
+  $ionicModal.fromTemplateUrl('templates/task.html',{
+    scope:$scope
+  }).then(function(modal){
+    $scope.modal = modal;
+  });
+
+});
